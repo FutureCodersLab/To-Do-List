8ed22b2a6707298788bab29c23daaf545e500454
nomuk984 <nomundari0804@gmail.com>
2025-02-11 22:43:23 +0800

    halfway-checkpoint

--- script.js
+++ script.js
@@ -1,139 +1,25 @@
+import { getTaskStructure } from "./structure.js";
 let tasks = [];
 
-document.addEventListener("DOMContentLoaded", () => {
-    const storedTasks = JSON.parse(localStorage.getItem("tasks"));
-    if (storedTasks) {
-        storedTasks.forEach((task) => tasks.push(task));
-        updateTaskList();
-        updateStats();
-    }
-});
-const saveTasks = () => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-};
+const input = document.getElementById("input");
+const addTaskButton = document.getElementById("add-task");
+const taskList = document.getElementById("task-list");
 
 const addTask = () => {
-    const taskInput = document.getElementById("task-input");
-    const text = taskInput.value.trim();
-
-    if (text) {
-        tasks.push({ text: text, completed: false });
-
-        updateTaskList();
-    }
-    taskInput.value = "";
-    updateStats();
-    saveTasks();
-};
-const toggleTaskComplete = (index) => {
-    tasks[index].completed = !tasks[index].completed;
+    const text = input.value.trim();
+    if (!text) return;
+    tasks.push({ text, isComplete: false });
+    input.value = "";
     updateTaskList();
-    updateStats();
-    saveTasks();
-};
-
-const deleteTask = (index) => {
-    tasks.splice(index, 1);
-    updateTaskList();
-    updateStats();
-    saveTasks();
-};
-const editTask = (index) => {
-    const taskInput = document.getElementById("task-input");
-    taskInput.value = tasks[index].text;
-
-    tasks.splice(index, 1);
-    updateTaskList();
-    updateStats();
-    saveTasks();
-};
-const updateStats = () => {
-    const completedTasks = tasks.filter((task) => task.completed).length;
-    const totalTasks = tasks.length;
-    const progress = (completedTasks / totalTasks) * 100;
-    const progressBar = document.getElementById("progress");
-
-    progressBar.style.width = `${progress}%`;
-
-    document.getElementById(
-        "numbers"
-    ).innerText = `${completedTasks} / ${totalTasks}`;
-
-    if (tasks.length && completedTasks === totalTasks) {
-        blastConfetti();
-    }
 };
 
 const updateTaskList = () => {
-    const taskList = document.getElementById("task-list");
     taskList.innerHTML = "";
-
     tasks.forEach((task, index) => {
-        const listItem = document.createElement("li");
-
-        listItem.innerHTML = `
-        <div class = "task-item">
-            <div class = "task ${task.completed ? "completed" : ""}">
-                <input type="checkbox" class="checkbox" ${
-                    task.completed ? "checked" : ""
-                } />
-                <span> ${task.text}</span>
-            </div>
-            <div class = "icons">
-                <img src="./img/edit.png" onClick = "editTask(${index})"/>
-                <img src="./img/delete.png" onClick = "deleteTask(${index})"/>
-            </div>
-        </div>
-        `;
-        listItem
-            .querySelector(".checkbox")
-            .addEventListener("change", () => toggleTaskComplete(index));
-        taskList.appendChild(listItem);
+        const li = document.createElement("li");
+        li.className = `task ${task.isComplete ? "completed" : ""}`;
+        li.innerHTML = getTaskStructure(task, index);
+        taskList.appendChild(li);
     });
 };
-document.getElementById("new-task").addEventListener("click", function (e) {
-    e.preventDefault();
-    addTask();
-});
-
-const blastConfetti = () => {
-    const count = 200,
-        defaults = {
-            origin: { y: 0.7 },
-        };
-
-    function fire(particleRatio, opts) {
-        confetti(
-            Object.assign({}, defaults, opts, {
-                particleCount: Math.floor(count * particleRatio),
-            })
-        );
-    }
-
-    fire(0.25, {
-        spread: 26,
-        startVelocity: 55,
-    });
-
-    fire(0.2, {
-        spread: 60,
-    });
-
-    fire(0.35, {
-        spread: 100,
-        decay: 0.91,
-        scalar: 0.8,
-    });
-
-    fire(0.1, {
-        spread: 120,
-        startVelocity: 25,
-        decay: 0.92,
-        scalar: 1.2,
-    });
-
-    fire(0.1, {
-        spread: 120,
-        startVelocity: 45,
-    });
-};
+addTaskButton.addEventListener("click", addTask);
